adf392dea155b641b360bf6d989dff7b812bc9ff
Avinash Panchal <avinash0702panchal@gmail.com>
2024-08-20 01:46:36 +0530

    render list of users using array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,45 @@
-import Counter from "./Counter"
+import Card from "./Card.jsx";
+import './index.css'
 
 function App() {
 
+  const users = [
+    {
+      name : 'Alexa',
+      age : 22,
+      gmail :'Alexa@gmail.com',
+      
+    },
+    {
+      name : 'Delba',
+      age : 28,
+      gmail :'Delba@gmail.com'
+    },
+    {
+      name : 'Alia',
+      age : 25,
+      gmail :'Alia@gmail.com'
+    },
+    {
+      name : 'Sana',
+      age : 30,
+      gmail :'Sana@gmail.com'
+    },
+    {
+      name : 'Maria',
+      age : 18,
+      gmail :'Maria@gmail.com'
+    }
+  ]
+
+
   return (
    <>
-    <Counter/>
+   {
+    users.map((user)=>{
+      return <Card name={user.name}/>
+    })
+   }
    </>
   )
 }
--- frontend/src/Counter.jsx
+++ frontend/src/Counter.jsx
@@ -1,24 +1,29 @@
 import { useState } from "react";
 
-function Counter(){
-    let [counter, setCounter ] = useState(0);
-    let changeCounter = (val)=>{
+function Counter() {
+    let [counter, setCounter] = useState(0);
+    let changeCounter = (val) => {
+
         setCounter(counter + val)
     }
     return (
-        <>
-           <div className="counter">
+        <>/
+            <h1 className="bg-green-400 text-black p-4 rounded-xl text-center mb-4"> Tailwind CSS</h1>
+            
+
+
+            {/* <div className="counter">
             This is {counter}
            </div>
-           <button className="add" onClick={()=>changeCounter(1)}>
+           <button className="bg-green-400 text-black p-4 rounded-xl" onClick={()=>{ if(counter < 20) changeCounter(1)}}>
              add 
            </button>
-           <button className="delete" onClick={()=> changeCounter(-1)}>
+           <button className="delete" onClick={()=>{ if(counter > 0) changeCounter(-1)}}>
             delete
            </button>
            <p>
             this is chai {counter}
-           </p>
+           </p> */}
         </>
     )
 }
